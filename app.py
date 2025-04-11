e3176cd5848da43b07e0e55f1956021433b85dcc
gauravo7 <gaurav@o7services.com>
2025-04-11 09:43:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,5 +9,10 @@ def hello():                      # call method hello
 @app.route("/about")                   # at the end point /
 def about():                      # call method hello
     return "<h2>Welcome my Flask App</h2>"         # which returns "hello world"
+
+@app.route("/contact")                   # at the end point /
+def kuchv():                      # call method hello
+    return "<h2>My Contact page</h2>"         # which returns "hello world"
+
 if __name__ == "__main__":        # on running python app.py
     app.run() 
