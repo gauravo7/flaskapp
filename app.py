fefada1e60f7afd3d5bb788f57899a782e6f9173
gauravo7 <gaurav@o7services.com>
2025-04-11 09:36:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,5 +5,9 @@ app = Flask(__name__)             # create an app instance
 @app.route("/")                   # at the end point /
 def hello():                      # call method hello
     return "Hello World!"         # which returns "hello world"
+
+@app.route("/about")                   # at the end point /
+def about():                      # call method hello
+    return "<h2>Welcome my Flask App</h2>"         # which returns "hello world"
 if __name__ == "__main__":        # on running python app.py
     app.run() 
